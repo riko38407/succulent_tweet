ad5391aa4affa385c750f69a4f688c15fdabf812
riko38407 <namakemono0711@yahoo.co.jp>
2022-10-07 18:01:35 +0900

    マイグレーションファイルを編集

--- db/migrate/20221005060558_create_tweets.rb
+++ db/migrate/20221005060558_create_tweets.rb
@@ -1,7 +1,7 @@
 class CreateTweets < ActiveRecord::Migration[6.0]
   def change
     create_table :tweets do |t|
-      t.string :user, null: false,foreign_key: true
+      t.references :user, null: false,foreign_key: true
       t.string :text, null: false 
       t.timestamps
     end
