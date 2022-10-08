a4f6cb67db9ed99f3047f070ad99f803211b39e6
riko38407 <namakemono0711@yahoo.co.jp>
2022-10-08 15:24:46 +0900

    バリデーションを追加

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,10 @@
 class Tweet < ApplicationRecord
+  belongs_to :user
   has_one_attached :image
 
-  validates :text, presence: true
-  validates :image, presence: true
+  with_options presence: true do
+  validates :text
+  validates :image
+  validates :user_id
+  end
 end
