2d4034ddc83648344d405c5efe2fd157aa192564
riko38407 <namakemono0711@yahoo.co.jp>
2022-10-10 16:16:45 +0900

    tweetテーブルを再作成

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,11 +1,12 @@
 class Tweet < ApplicationRecord
-  belongs_to :user, optional: true
-  has_one_attached :image
-
-  with_options presence: true do
-    validates :title
-    validates :text
-    validates :image
-    validates :user_id
+    belongs_to :user, optional: true
+    has_one_attached :image
+  
+    with_options presence: true do
+      validates :title
+      validates :text
+      validates :image
+      validates :user_id
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_07_085224) do
+ActiveRecord::Schema.define(version: 2022_10_10_071311) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
