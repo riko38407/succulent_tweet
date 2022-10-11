828da33c4ea30f28a0cb3d412f83e50940cd3a6f
riko38407 <namakemono0711@yahoo.co.jp>
2022-10-11 11:51:05 +0900

    edit,updateアクションを追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,8 @@
 class TweetsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_prototype, except: [ :new, :destroy, :show]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+
   def index
     @tweets = Tweet.includes(:user).limit(12)
    
@@ -28,9 +31,33 @@ class TweetsController < ApplicationController
     @tweet = Tweet.find(params[:id])
   end
 
+def edit
+    unless user_signed_in?
+      redirect_to action: :edit
+    end
+  end
+
+  def update
+    if @tweet.update(tweet_params)
+     redirect_to tweet_path(@tweet)
+     else
+    
+     render :edit
+     end
+   
+   end
+
   private
 
   def tweet_params
     params.require(:tweet).permit(:title, :text, :image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @tweet.user
   end
 end
