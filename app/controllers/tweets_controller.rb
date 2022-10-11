f2efcb5c82b9c5f080445373001ae3a57e5a54bc
riko38407 <namakemono0711@yahoo.co.jp>
2022-10-11 12:47:50 +0900

    ツイート詳細編集機実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,16 +1,16 @@
 class TweetsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_prototype, except: [ :new, :destroy, :show]
+  before_action :set_tweet, except: [ :index, :new, :create]
   before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
   def index
-    @tweets = Tweet.includes(:user).limit(12)
+    @tweets = Tweet.all.order('created_at DESC')
    
   end
 
   def new
     @tweet = Tweet.new
-    @tweet = Tweet.find(params[:id])
+   
   end
 
   def create
@@ -23,29 +23,25 @@ class TweetsController < ApplicationController
   end
   
   def destroy
-    @tweet = Tweet.find(params[:id])
     @tweet.destroy
   end
 
   def show
-    @tweet = Tweet.find(params[:id])
   end
 
 def edit
-    unless user_signed_in?
-      redirect_to action: :edit
-    end
+  unless user_signed_in?
+    redirect_to action: :edit
   end
+end
 
   def update
     if @tweet.update(tweet_params)
-     redirect_to tweet_path(@tweet)
-     else
-    
-     render :edit
-     end
-   
-   end
+      render 'show'
+    else
+      render 'edit'
+    end
+  end
 
   private
 
@@ -53,8 +49,8 @@ def edit
     params.require(:tweet).permit(:title, :text, :image).merge(user_id: current_user.id)
   end
 
-  def set_prototype
-    @prototype = Prototype.find(params[:id])
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
   end
 
   def contributor_confirmation
