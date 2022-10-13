dffd5d5fd777aa9047c12e85aa067a56308bb21b
riko38407 <namakemono0711@yahoo.co.jp>
2022-10-13 12:37:57 +0900

    画像を変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,6 @@ require("@rails/ujs").start()
 
 require("@rails/activestorage").start()
 require("channels")
-require("../jquery-3.6.0.min");
-require("../style")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
