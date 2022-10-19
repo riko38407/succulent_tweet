d0f85be22ed6b1304c925972076f8de7ab0f7e39
riko38407 <namakemono0711@yahoo.co.jp>
2022-10-19 16:01:57 +0900

    コードを修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,13 @@
 class CommentsController < ApplicationController
   
   def create
-<<<<<<< Updated upstream
       @comment = Comment.new(comment_params)
       @tweet = Tweet.find(params[:tweet_id])
       if @comment.save
         CommentChannel.broadcast_to @tweet, { comment: @comment, user: @comment.user }
         end
       end
-=======
-    @comment = Comment.new(comment_params)
-    @tweet = Tweet.find(params[:tweet_id])
-    if @comment.save
-      CommentChannel.broadcast_to @tweet, { comment: @comment, user: @comment.user }
-    end
-  end
- 
->>>>>>> Stashed changes
+
 
   private
 
