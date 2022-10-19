375820f74ba0c9e7b334fdaadbb26853468f9fb6
riko38407 <namakemono0711@yahoo.co.jp>
2022-10-19 21:09:48 +0900

    userモデル単体テストコード記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :tweets
   has_many :comments
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+
+  with_options presence: true do
+    validates :name, presence: true
+    VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥々]+\z/
+  end
 end
