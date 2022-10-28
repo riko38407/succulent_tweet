4c96cc83edcdee4239f23ce6736eb2a9547e6d96
riko38407 <namakemono0711@yahoo.co.jp>
2022-10-28 15:17:48 +0900

    tweetモデル単体テスト実行

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,5 @@
 class Tweet < ApplicationRecord
-  belongs_to :user, optional: true
+  belongs_to :user
   has_one_attached :image
   has_many :comments, dependent: :destroy
 
@@ -7,6 +7,5 @@ class Tweet < ApplicationRecord
     validates :title
     validates :text
     validates :image
-    validates :user_id
   end
 end
